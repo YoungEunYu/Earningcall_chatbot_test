c0839eb9cc6e269292b2ebcd69e49c1fd5cef6d1
Youngeun <jgjrjej@ewhain.net>
2024-12-23 17:14:40 -0500

    streamlit web test

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="chatGPT API developmet web",
+    page_icon="📈"
+)
+
+st.title("EarningCall Card with chatGPT")
+st.subheader("subhearder_test")
